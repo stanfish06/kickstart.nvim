86b5adfa57897d34e7d6b1ba4dcb7beb9062ead5
Stan <zyyu@umich.edu>
2025-05-29 14:12:33 -0400

    update python format

--- init.lua
+++ init.lua
@@ -811,7 +811,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'ruff' },
+        python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
