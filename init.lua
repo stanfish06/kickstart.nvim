3ec25995e007f4476385129eae01a13475e3cac2
Stan <zyyu@umich.edu>
2025-10-17 19:14:18 -0400

    update

--- init.lua
+++ init.lua
@@ -179,6 +179,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- neotree and oil
 -- vim.keymap.set('n', '\\', '<cmd>Neotree<CR>')
 vim.keymap.set('n', '-', '<cmd>Oil<CR>')
+-- mini file
+vim.keymap.set('n', '\\', function()
+  require('mini.files').open()
+end)
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
