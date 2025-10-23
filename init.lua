e41c18092f847a822a969081e0d9375ee6cd551a
Stan <zhiyuanyucleveland@outlook.com>
2025-10-23 09:13:25 -0400

    update

--- init.lua
+++ init.lua
@@ -1381,6 +1381,7 @@ require('lazy').setup({
         bashls = {},
         ty = {},
         zls = {},
+        markdownlint = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
